cd798fabde9bd9c871d56f460a16fa807510d3e8
LNUitTutor <lnu.it.tutor@gmail.com>
2023-02-20 16:40:20 +0200

    fix bug

--- GitDemo/Functions.cpp
+++ GitDemo/Functions.cpp
@@ -3,20 +3,19 @@
 
 void SelectSort(int a[], int n)
 {
-	/*for (int i = 0; i < n; ++i)
+	// i define index for current max value
+	for (int i = n - 1; i >= 1; --i)
 	{
-		a[i] = i;
-	}*/
-	for (int i = n; i > 1; --i)
-	{
+		// find max and it's index
 		int max = a[0];
 		int no = 0;
-		for (int j = 1; j < i; ++j)
+		for (int j = 1; j <= i; ++j)
 			if (a[j] > max)
 			{
 				max = a[j];
 				no = j;
 			}
+		// excange values
 		if (i != no)
 		{
 			a[no] = a[i];
