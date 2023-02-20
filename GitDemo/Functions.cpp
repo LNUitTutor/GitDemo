1917849166880c4db0ffde76a759aa4f26bbbf8c
LNUitTutor <lnu.it.tutor@gmail.com>
2023-02-20 16:00:11 +0200

    Change sort algorithm (with error)

--- GitDemo/Functions.cpp
+++ GitDemo/Functions.cpp
@@ -3,9 +3,25 @@
 
 void SelectSort(int a[], int n)
 {
-	for (int i = 0; i < n; ++i)
+	/*for (int i = 0; i < n; ++i)
 	{
 		a[i] = i;
+	}*/
+	for (int i = n; i > 1; --i)
+	{
+		int max = a[0];
+		int no = 0;
+		for (int j = 1; j < i; ++j)
+			if (a[j] > max)
+			{
+				max = a[j];
+				no = j;
+			}
+		if (i != no)
+		{
+			a[no] = a[i];
+			a[i] = max;
+		}
 	}
 }
 
